44a1ad4787e241e2115180a5508d8f963816f8ae
Ryan Wilson <rwils83@gmail.com>
2021-12-01 09:16:59 -0500

    Adding download

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, send_file
+import logging
 app = Flask("app")
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+@app.route("/roadedgesaves", methods=["GET"])
+def download():
+    return send_file("/home/edgecase/.config/Epic/FactoryGame/Saved/SaveGames/server/testing_autosave_0.sav", as_attachment=True)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s: %(message)s')
     app.run(debug=True, port=80)
